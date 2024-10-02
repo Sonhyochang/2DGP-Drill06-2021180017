fd2142edf9f930cdfeadd11caa22584423ad1ec4
Hyochang-Son@main notebook <dagdf20@naver.com>
2024-10-02 14:58:02 +0900

    random 모듈, hand_arrow 이미지 추가

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -1,10 +1,13 @@
 from pico2d import *
+import random
+
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
+Hand_arrow = load_image('hand_arrow.png')
 
 
 def handle_events():
@@ -14,8 +17,6 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
