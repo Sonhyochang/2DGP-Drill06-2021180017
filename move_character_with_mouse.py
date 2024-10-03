e8b44b0df4eb5730c3da0efc51463456bf5665ed
Hyochang-Son@main notebook <dagdf20@naver.com>
2024-10-03 17:22:12 +0900

    상 하 좌 우 로 커서 따라 이동하는 함수 구현

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -9,6 +9,13 @@ TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 Hand_arrow = load_image('hand_arrow.png')
 
+running = True
+x1, y1 = TUK_WIDTH // 2, TUK_HEIGHT // 2
+frame = 0
+hide_cursor()
+
+new_x = random.randint(1,TUK_WIDTH)
+new_y = random.randint(1,TUK_HEIGHT)
 
 def handle_events():
     global running
@@ -22,25 +29,48 @@ def handle_events():
     pass
 
 
-
-running = True
-x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-frame = 0
-hide_cursor()
-
-new_x = random.randint(1,TUK_WIDTH)
-new_y = random.randint(1,TUK_HEIGHT)
-
-print(new_x)
-print(new_y)
-while running:
+def draw_master():
+    global frame
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    Hand_arrow.draw(new_x,new_y)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    Hand_arrow.draw(new_x, new_y)  # 랜덤 위치 손
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x1, y1)
     update_canvas()
     frame = (frame + 1) % 8
+    delay(0.05)
     handle_events()
+
+def trace_cursor():
+    global x1,y1,new_x,new_y,frame
+
+    if new_x > x1 and new_y > y1 :
+        a = (new_y-y1)/(new_x-x1)
+        b = y1 - x1 * a
+        for x1 in range(x1,new_x+1,10):
+            y1 = a * x1 + b
+            draw_master()
+    elif new_x < x1 and new_y > y1:
+        a = (new_y - y1) / (new_x - x1)
+        b = y1 - x1 * a
+        for x1 in range(x1, new_x + 1, -10):
+            y1 = a * x1 + b
+            draw_master()
+    elif new_x < x1 and new_y < y1:
+        a = (new_y - y1) / (new_x - x1)
+        b = y1 - x1 * a
+        for x1 in range(x1, new_x + 1, -10):
+            y1 = a * x1 + b
+            draw_master()
+    elif new_x > x1 and new_y < y1:
+        a = (new_y - y1) / (new_x - x1)
+        b = y1 - x1 * a
+        for x1 in range(x1, new_x + 1, 10):
+            y1 = a * x1 + b
+            draw_master()
+
+while running:
+
+    trace_cursor()
 
 close_canvas()
 
